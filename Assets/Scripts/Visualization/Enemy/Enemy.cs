6a9e06b8546e8d7e64bcf723174f72dfda16966a
Juho Selenius <juho.selenius@metropolia.fi>
2023-04-15 18:40:05 +0300

    Merge commit

--- Assets/Scripts/Visualization/Enemy/Enemy.cs
+++ Assets/Scripts/Visualization/Enemy/Enemy.cs
@@ -1,21 +1,27 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using Logic.Game;
 
 namespace Visualization
 {
     public class Enemy : MonoBehaviour
     {
+        private IGameManager _gameManager;
         public float maxHealth;
         [SerializeField] private float health;
         public float meleeDamage;
-        public Rigidbody rb;
-        public float cooldownTimer;
-        [SerializeField] private float cooldown = 5f;
+
+        private void Awake()
+        {
+            _gameManager = ServiceLocator.GetService<IGameManager>();
+
+        }
         void Start()
         {
-            rb = GetComponent<Rigidbody>();
+            
             health = maxHealth;
         }
+
+        
 
         private void OnCollisionEnter(Collision collision)
         {
@@ -23,15 +29,6 @@ namespace Visualization
             if(collision.gameObject.tag == "PlayerProjectile")
             {
                 health -= collision.gameObject.GetComponent<Projectile>().damage;
-            }
-            else if(collision.gameObject.tag == "IceBullet")
-            {
-                health -= collision.gameObject.GetComponent<Projectile>().damage;
-                rb.isKinematic = true;
-                cooldownTimer -= Time.deltaTime;
-                if (cooldownTimer > 0) return;
-                cooldownTimer = cooldown;
-                rb.isKinematic = false;
             }
 
             if (health <= 0)
@@ -43,7 +40,9 @@ namespace Visualization
         private void EnemyDie()
         {
             // Here code for what happens when the enemy dies
+            _gameManager.SetallEnemiesCleared();
             FindObjectOfType<AudioManager>().Play("EnemyDeath");
+            FindObjectOfType<AudioManager>().StopPlay("EnemyWalk");
             Destroy(gameObject);
         }
     }
