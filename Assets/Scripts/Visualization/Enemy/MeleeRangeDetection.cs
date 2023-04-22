9748ef0ef469bdc1831af9c5ccc6aebca7839788
Severi Pietikäinen <severi.pietikainen1@gmail.com>
2023-04-22 20:42:43 +0300

    Skeleton enemies, trap script

--- Assets/Scripts/Visualization/Enemy/MeleeRangeDetection.cs
+++ Assets/Scripts/Visualization/Enemy/MeleeRangeDetection.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class MeleeRangeDetection : MonoBehaviour
 {
     public SkeletonEnemy skelly;
+    public SmallSkeletonController smallSkelly;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
             skelly.playerInAttackRange = true;
+            smallSkelly.playerInAttackRange = true;
         }
     }
 
