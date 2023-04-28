9b41bc1af8b3b5e676fff1b87765b8f5f7d8e822
Juho Selenius <juho.selenius@metropolia.fi>
2023-04-28 10:33:52 +0300

    Added Level Complete Scene

--- Assets/Scripts/Visualization/UI/LevelCompleteControl.cs
+++ Assets/Scripts/Visualization/UI/LevelCompleteControl.cs
@@ -1,18 +1,36 @@
+using Logic.Game;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelCompleteControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI thisRoundTime;
+    public TextMeshProUGUI bestTime;
+
+    private IGameManager _gameManager;
+
+    private void Awake()
     {
-        
+        _gameManager = ServiceLocator.GetService<IGameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        thisRoundTime.text = _gameManager.GetLevel2CurrentTime().ToString();
+        bestTime.text = _gameManager.GetLevel2BestTime().ToString();
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadLevel3()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        SceneManager.LoadScene("Level3");
     }
 }
