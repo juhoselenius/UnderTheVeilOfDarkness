e47ccde85c6686d344f5a341cb55294fedb3ee3f
Juho Selenius <juho.selenius@metropolia.fi>
2023-04-30 09:03:38 +0300

    Changes to Pause Menu

--- Assets/Scripts/Visualization/UI/PauseMenuControl.cs
+++ Assets/Scripts/Visualization/UI/PauseMenuControl.cs
@@ -1,54 +1,89 @@
 using Logic.Game;
-using System.Collections;
-using System.Collections.Generic;
+using Logic.Player;
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class PauseMenuControl : MonoBehaviour
+namespace Visualization
 {
-    public GameObject pauseMenu;
-    private IGameManager _gameManager;
-
-    private void Awake()
+    public class PauseMenuControl : MonoBehaviour
     {
-        _gameManager = ServiceLocator.GetService<IGameManager>();
-    }
+        private IPlayerManager _playerManager;
+        private IGameManager _gameManager;
+    
+        public GameObject pauseMenu;
 
-    void Start()
-    {
-        pauseMenu.SetActive(false);
-    }
+        public Slider sightSlider;
+        public Slider hearingSlider;
+        public Slider movementSlider;
+        public Slider attackSlider;
+        public Slider defenseSlider;
 
-    void Update()
-    {
-        // Toggling Pause Menu on and off
-        if (Input.GetKeyDown(KeyCode.Escape))
+        public TextMeshProUGUI sightValueText;
+        public TextMeshProUGUI hearingValueText;
+        public TextMeshProUGUI movementValueText;
+        public TextMeshProUGUI attackValueText;
+        public TextMeshProUGUI defenseValueText;
+
+        private void Awake()
         {
-            if (!pauseMenu.activeInHierarchy)
+            _playerManager = ServiceLocator.GetService<IPlayerManager>();
+            _gameManager = ServiceLocator.GetService<IGameManager>();
+        }
+
+        void Start()
+        {
+            pauseMenu.SetActive(false);
+            UpdateSliderValues();
+        }
+
+        void Update()
+        {
+            // Toggling Pause Menu on and off
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                Pause();
-                pauseMenu.SetActive(true);
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-            else
-            {
-                Resume();
-                pauseMenu.SetActive(false);
-                Cursor.lockState = CursorLockMode.Locked;
+                if (!pauseMenu.activeInHierarchy)
+                {
+                    Pause();
+                    UpdateSliderValues();
+                    pauseMenu.SetActive(true);
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                }
+                else
+                {
+                    Resume();
+                    pauseMenu.SetActive(false);
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
             }
         }
-    }
 
-    private void Pause()
-    {
-        Time.timeScale = 0;
-        _gameManager.SetGamePaused();
-    }
+        private void Pause()
+        {
+            Time.timeScale = 0;
+            _gameManager.SetGamePaused();
+        }
 
-    private void Resume()
-    {
-        Time.timeScale = 1;
-        _gameManager.SetGamePaused();
+        private void Resume()
+        {
+            Time.timeScale = 1;
+            _gameManager.SetGamePaused();
+        }
+
+        private void UpdateSliderValues()
+        {
+            defenseSlider.value = _playerManager.GetDefense();
+            attackSlider.value = _playerManager.GetAttack();
+            movementSlider.value = _playerManager.GetMovement();
+            hearingSlider.value = _playerManager.GetHearing();
+            sightSlider.value = _playerManager.GetSight();
+        }
+
+        public void DropdownMenuChange(int value)
+        {
+            _playerManager.ChangePreset(value);
+            UpdateSliderValues();
+        }
     }
 }
